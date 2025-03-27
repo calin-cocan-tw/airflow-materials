d78125b7818f00dc92d644253043e527ba6127df
Calin Cocan <calin.cocan@throughtworks.com>
2025-03-27 10:49:29 +0200

    chore: my own changes during training session

--- airflow-section-9/mnt/airflow/dags/marketing_dag copy.py
+++ airflow-section-9/mnt/airflow/dags/marketing_dag copy.py
@@ -6,17 +6,17 @@ from airflow.operators.dummy_operator import DummyOperator
 from datetime import datetime, timedelta
 
 default_args = {
-        "owner": "airflow", 
+        "owner": "admin",
         "start_date": airflow.utils.dates.days_ago(1)
     }
 
-with DAG(dag_id="logger_dag", default_args=default_args, schedule_interval="@daily") as dag:
+with DAG(dag_id="admin_dag", default_args=default_args, schedule_interval="@daily") as dag:
 
     t1 = DummyOperator(task_id="t1")
 
     t2 = BashOperator(
             task_id="t2",
-            bash_command="sleep 10"
+            bash_command="echo 'It works'"
         )
 
     t1 >> t2
